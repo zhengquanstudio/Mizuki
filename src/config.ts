f1dd8f9d5cb5d3918dcee59fc8ad489ba292a8a4
zhengquanstudio <laisy2089@163.com>
2026-04-23 02:36:56 +0800

    chore(content): sync main@22a0d1d

--- src/config.ts
+++ src/config.ts
@@ -21,23 +21,13 @@ import { LinkPreset } from "./types/config";
 // 移除i18n导入以避免循环依赖
 
 // 定义站点语言
-<<<<<<< HEAD
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "郑全散记のBLOG",
-	subtitle: "One demo website",
+	title: "郑全の散记BLOG",
+	subtitle: "zheng quan blog",
 	siteURL: "https://www.zhengquan.org/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-04-22", // 站点开始运行日期，用于站点统计组件计算运行天数
-=======
-//const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
-//const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
-//export const siteConfig: SiteConfig = {
-//	title: "郑全散记のBLOG",
-//	subtitle: "One demo website",
-//	siteURL: "https://www.zhengquan.org/", // 请替换为你的站点URL，以斜杠结尾
-//	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
->>>>>>> 7072c86fa6fe257f85e102a7bd3ea1d0c53c2b44
 
 	timeZone: SITE_TIMEZONE,
 
